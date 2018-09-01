02bb472818b3b7f15c200a13e0091806dc52c27f
Lachlan Phillips <flipsidelachy@gmail.com>
2018-09-01 14:25:43 +0930

    Initial commit with working variables and midi commands

---
+++ index.js
@@ -0,0 +1,295 @@
+const byline = require('byline');
+const childProcess = require('child_process');
+const execFile = childProcess.execFile;
+const spawn = childProcess.spawn;
+const denodeify = require('denodeify');
+const fs = require('fs');
+const tmp = require('tmp');
+const promisify = require('util').promisify;
+
+const Constant = {
+  in: [],
+  out: ['out'],
+  params: ['value'],
+  storage: null,
+  init: null,
+  process: 'float %%out%% = %%value%%;',
+  isDirect: true,
+}
+
+const Variable = {
+  in: [],
+  out: ['out'],
+  params: ['value', 'varId'],
+  storage: null,
+  init: 'vars[%%varId%%] = %%value%%;',
+  process: 'float %%out%% = vars[%%varId%%];',
+  isDirect: true,
+}
+
+const SineWave = {
+  in: ['period'],
+  out: ['out'],
+  params: [],
+  storage: 'int %%id%%_tick;',
+  init: '%%id%%_tick = 0;',
+  process: '%%id%%_tick++;\nfloat %%out%% = sin(%%id%%_tick / %%period%%) * 0.02f;',
+  isDirect: true,
+};
+
+class Engine {
+  constructor (model, process) {
+    this._model = model;
+    this._process = process;
+
+    byline.createStream(process.stdout).on('data', line => {
+      const message = JSON.parse(line);
+      this.handle(message);
+    });
+    byline.createStream(process.stderr).on('data', line => {
+      console.warn('WARN: ' + line);
+    });
+
+    model._eventSend = msg => {
+      process.stdin.write(msg + '\n');
+    }
+  }
+
+  handle (msg) {
+    // console.log('Engine:', msg);
+
+    let type = msg.msg;
+    if (type === 'midi') type += '.' + msg.type;
+
+    this._model._fire(type, msg);
+  }
+
+  async waitForExit () {
+    await onExit(this._process);
+  }
+}
+
+class Model {
+  constructor () {
+    this.out = [];
+    this.nodes = [];
+    this._idCounter = 0;
+    this._outPortCounter = 0;
+    this._eventListeners = [];
+    this._eventSend = null;
+    this._varCount = 0;
+  }
+
+  addNode (info, params) {
+    if (info === Variable) throw new TypeError('A Variable must be instantiated with the addVariable method');
+    const node = this._createNode(info, params);
+    this.nodes.push(node);
+    return node;
+  }
+
+  addVariable (name, initialValue) {
+    const varId = this._varCount++;
+    const node = this._createNode(Variable, { value: initialValue, varId });
+    node.set = value => {
+      this._send(`set ${varId} ${value}`);
+    }
+    this.nodes.push(node);
+    return node;
+  }
+
+  connect (from, to) {
+    to.push(from);
+  }
+
+  onNote (callback) {
+    this._eventListeners.push(['midi.note-down', msg => callback(msg.note, msg.velocity)]);
+  }
+
+  _createNode (info, params) {
+    const id = this._idCounter++;
+    const node = {
+      _id: id,
+      _info: info,
+      _params: params,
+      _inputs: new Map(),
+      _outputs: new Map()
+    };
+    info.in.forEach(name => {
+      const port = []
+      node[name] = port;
+      node._inputs.set(name, port)
+    })
+    info.out.forEach(name => {
+      const id = this._outPortCounter++;
+      node[name] = id;
+      node._outputs.set(name, id)
+    })
+    return node;
+  }
+
+  _fire (type, msg) {
+    this._eventListeners.forEach(l => {
+      if (l[0] !== type) return;
+      try {
+        l[1](msg);
+      } catch (e) {
+        console.error(e);
+      }
+    })
+  }
+
+  _send (msg) {
+    if (this._eventSend === null) console.error('EventSend is not initialised');
+    else this._eventSend(msg);
+  }
+
+  _initEventSend (fn) {
+    this._eventSend = fn;
+  }
+}
+
+async function buildAndStartEngine (model) {
+  const template = await denodeify(fs.readFile)('templates/main.c', 'utf8');
+  const source = generateEngineSource(template, model);
+
+  const tmpSourceFile = await denodeify(tmp.file)({
+    discardDescriptor: true,
+    prefix: 'codegen-synth-',
+    postfix: '.c',
+  })
+  const tmpBinaryFile = await denodeify(tmp.file)({
+    discardDescriptor: true,
+    prefix: 'codegen-synth-',
+    postfix: '',
+  })
+
+  await denodeify(fs.writeFile)(tmpSourceFile, source, 'utf8')
+
+  try {
+    await promisify(execFile)('gcc', [
+      '-std=c11',
+      '-Wall', '-Werror',
+      '-O3',
+      '-o', tmpBinaryFile,
+      tmpSourceFile,
+      '-lm', '-ljack', '-lpthread',
+    ]);
+
+    const engineProcess = spawn(tmpBinaryFile, [], {
+      stdio: ['pipe', 'pipe', 'inherit']
+    });
+    return new Engine(model, engineProcess);
+  } catch (e) {
+    if (e.stderr) throw new Error('Failed to compile source: ' + e.stderr.trim());
+    else throw e;
+  }
+}
+
+function generateEngineSource (template, model) {
+  const formatKeyInline = (template, key, value) => {
+    const regex = new RegExp(`%%${key}%%`, 'g');
+    return template.replace(regex, value);
+  };
+  const formatKeyBlock = (template, key, value) => {
+    const regex = new RegExp(`^.*%%${key.toUpperCase()}%%.*$`, 'gm');
+    return template.replace(regex, `/* BEGIN ${key} */\n${value}\n/* END ${key} */`);
+  }
+  const formatInline = (template, values) => {
+    let formatted = template;
+    for (const key in values) {
+      formatted = formatKeyInline(formatted, key, values[key]);
+    }
+    return formatted;
+  };
+  const formatBlocks = (template, values) => {
+    let formatted = template;
+    for (const key in values) {
+      formatted = formatKeyBlock(formatted, key, values[key]);
+    }
+    return formatted;
+  };
+
+  const formatNodeId = id => 'n' + id;
+  const formatPortVar = id => 'p' + id;
+  const formatPortSum = ports => {
+    if (ports.length === 0) {
+      return '0';
+    } else if (ports.length === 1) {
+      return formatPortVar(ports[0]);
+    } else {
+      return '(' + ports.map(formatPortVar).join(' + ') + ')';
+    }
+  };
+
+  const formatNodeSections = node => {
+    const vars = {
+      id: formatNodeId(node._id)
+    }
+    node._info.params.forEach(key => {
+      console.log('x', key, node._params[key]);
+      vars[key] = node._params[key];
+    });
+    node._inputs.forEach((value, key) => {
+      vars[key] = formatPortSum(value);
+    });
+    node._outputs.forEach((value, key) => {
+      vars[key] = formatPortVar(value);
+    });
+    return {
+      storage: node._info.storage && formatInline(node._info.storage, vars),
+      init: node._info.init && formatInline(node._info.init, vars),
+      process: node._info.process && formatInline(node._info.process, vars)
+    }
+  };
+
+  const nodesSections = model.nodes.map(formatNodeSections);
+  const joinSections = sections => sections.filter(ss => ss).join('\n')
+  const sections = {
+    storage: joinSections(nodesSections.map(ss => ss.storage)),
+    init: joinSections(nodesSections.map(ss => ss.init)),
+    process: joinSections(nodesSections.map(ss => ss.process)),
+  }
+
+  const storagePrologue = `int VAR_COUNT = ${model._varCount};\nfloat vars[${model._varCount}];`;
+  const processEpilogue = formatKeyInline('return %%out%%;', 'out', formatPortSum(model.out));
+  sections.storage = storagePrologue + '\n' + sections.storage;
+  sections.process += '\n' + processEpilogue;
+
+  console.log(`storage:\n${sections.storage}\n`.replace(/\n/g, '\n  '));
+  console.log(`init:\n${sections.init}\n`.replace(/\n/g, '\n  '));
+  console.log(`process:\n${sections.process}\n`.replace(/\n/g, '\n  '));
+
+  return formatBlocks(template, sections);
+}
+
+async function main () {
+  const model = new Model();
+  const variable = model.addVariable('period', 18.0);
+  const sineWave = model.addNode(SineWave);
+  model.connect(variable.out, sineWave.period);
+  model.connect(sineWave.out, model.out);
+
+  model.onNote((note, velocity) => {
+    variable.set(20);
+  });
+
+  const engine = await buildAndStartEngine(model);
+  await engine.waitForExit();
+}
+
+function onExit(childProcess) {
+  return new Promise((resolve, reject) => {
+    childProcess.once('exit', (code, signal) => {
+      if (code === 0) {
+        resolve(undefined);
+      } else {
+        reject(new Error('Exit with error code: '+code));
+      }
+    });
+    childProcess.once('error', (err) => {
+      reject(err);
+    });
+  });
+}
+
+main().then(null, e => console.error('Crashed', e));
---
+++ templates/main.c
@@ -0,0 +1,159 @@
+#define _GNU_SOURCE
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <pthread.h>
+
+#include <jack/jack.h>
+#include <jack/midiport.h>
+
+jack_port_t *input_midi_port;
+jack_port_t *output_port;
+jack_client_t *client;
+
+int VAR_COUNT; float vars[0]; /* %%STORAGE%% */
+
+float make_sample() {
+  /* %%PROCESS%% */
+}
+
+int process (jack_nframes_t nframes, void *arg) {
+  void *midi_in = jack_port_get_buffer(input_midi_port, nframes);
+  int events = jack_midi_get_event_count(midi_in);
+  jack_midi_event_t event;
+  for (int i = 0; i < events; i++) {
+    if (jack_midi_event_get(&event, midi_in, i) != 0) continue;
+    if (event.size < 1) continue;
+
+    int type = event.buffer[0] & 0xF0;
+    int channel = event.buffer[0] & 0x0F;
+    if (type == 0xF0) {
+      type = channel;
+      printf("{\"msg\":\"midi\",\"type\":\"sys\",\"msg\":%d}\n", type);
+    } else if (type == 176) {
+      int controller = event.buffer[1];
+      int value = event.buffer[2];
+      printf("{\"msg\":\"midi\",\"type\":\"cc\",\"channel\":%d,\"controller\":%d,\"value\":%d}\n", channel, controller, value);
+    } else if (type == 128 || type == 144) {
+      printf("{\"msg\":\"midi\",\"type\":\"note-%s\",", type == 128 ? "up" : "down");
+      printf("\"channel\":%d,", channel);
+      printf("\"note\":%d,\"velocity\":%d}\n", event.buffer[1], event.buffer[2]);
+    } else {
+      printf("{\"msg\":\"midi\",\"type\":\"unknown:%d\",\"channel\":%d,\"extra\":[", type, channel);
+      for (int b = 1; b < event.size; b++) {
+        if (b == 1) printf("%d", (unsigned int) event.buffer[b]);
+        else printf(",%d", (unsigned int) event.buffer[b]);
+      }
+      printf("]}\n");
+    }
+  }
+
+  jack_default_audio_sample_t *out = (jack_default_audio_sample_t *) jack_port_get_buffer(output_port, nframes);
+  for (int i = 0; i < nframes; i++) {
+    out[i] = make_sample();
+  }
+  return 0;
+}
+
+/**
+ * JACK calls this shutdown_callback if the server ever shuts down or
+ * decides to disconnect the client.
+ */
+void jack_shutdown (void *arg) {
+  exit(1);
+}
+
+void *msg_thread(void *arg) {
+  fprintf(stderr, "Msg thread\n");
+  char *line = NULL;
+  size_t bufferLength = 0;
+  int lineLength = 0;
+  while ((lineLength = getline(&line, &bufferLength, stdin)) >= 0) {
+    int var = 0;
+    float value = 0;
+    if (sscanf(line, "set %d %f", &var, &value)) {
+      if (var >= 0 && var < VAR_COUNT) {
+        vars[var] = value;
+      } else {
+        fprintf(stderr, "invalid var set request: %d = %f\n", var, value);
+      }
+    }
+  }
+  free(line);
+  return NULL;
+}
+
+int main (int argc, char *argv[]) {
+  setbuf(stdout, NULL);
+
+  /* %%INIT%% */
+
+  pthread_t msg_thread_id;
+  if (pthread_create(&msg_thread_id, NULL, &msg_thread, NULL) != 0) {
+    fprintf(stderr, "Failed to start msg thread\n");
+    return 1;
+  }
+
+  const char **ports;
+  const char *client_name = "simple";
+  const char *server_name = NULL;
+  jack_options_t options = JackNullOption;
+  jack_status_t status;
+
+  /* open a client connection to the JACK server */
+  client = jack_client_open(client_name, options, &status, server_name);
+  if (client == NULL) {
+    printf("Failed to create client\n");
+    return 1;
+  }
+
+  jack_set_process_callback(client, process, 0);
+  jack_on_shutdown(client, jack_shutdown, 0);
+
+  printf("{\"msg\":\"start\",\"sample_rate\":%d}\n", jack_get_sample_rate(client));
+
+  output_port = jack_port_register(client, "output", JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+  if (output_port == NULL) {
+    printf("Failed to open an output port\n");
+    return 1;
+  }
+
+  input_midi_port = jack_port_register(client, "midi in", JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0);
+  if (input_midi_port == NULL) {
+    printf("Failed to open a MIDI port\n");
+    return 1;
+  }
+
+  if (jack_activate(client)) {
+    printf("Failed to active client\n");
+    return 1;
+  }
+
+  ports = jack_get_ports(client, NULL, NULL, JackPortIsInput);
+  int i = 0;
+  while (ports[i] != NULL) {
+    if (jack_connect (client, jack_port_name (output_port), ports[i])) {
+      // cerr << "cannot connect output ports" << endl;
+    }
+    i++;
+  }
+  free(ports);
+
+  ports = jack_get_ports(client, NULL, JACK_DEFAULT_MIDI_TYPE, JackPortIsOutput | JackPortIsPhysical);
+  i = 0;
+  while (ports[i] != NULL) {
+    if (jack_connect (client, ports[i], jack_port_name(input_midi_port))) {
+      printf("Cannot connect output ports\n");
+    }
+    i++;
+  }
+  free(ports);
+
+  /* keep running until stopped by the user */
+  sleep (-1);
+
+  jack_client_close(client);
+  return 0;
+}
